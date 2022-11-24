e26a326a9db1dd5ae8709c500505556d3db341e3
SAYEM5522 <md1040582@gmail.com>
2022-11-24 20:15:52 +0600

    Some Packages Add

--- apps/portfolio/tailwind.config.js
+++ apps/portfolio/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...require('ui/tailwind.config'),
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
